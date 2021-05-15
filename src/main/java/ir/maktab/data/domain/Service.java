b886a47fde5f1aff9c2e658e3c1e76104b96050e
zahra.asgari <zahra.asgari1996@yahoo.com>
2021-05-16 00:28:24 +0430

    change column name in single table for Service.java and SubService.java

--- src/main/java/ir/maktab/data/domain/Service.java
+++ src/main/java/ir/maktab/data/domain/Service.java
@@ -6,6 +6,8 @@ import javax.persistence.*;
 
 @Entity
 @Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(name = "Type_Of_Service")
+@DiscriminatorValue(value = "Service")
 public class Service {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
--- src/main/java/ir/maktab/data/domain/SubService.java
+++ src/main/java/ir/maktab/data/domain/SubService.java
@@ -1,9 +1,11 @@
 package ir.maktab.data.domain;
 
 import javax.persistence.Column;
+import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
 
 @Entity
+@DiscriminatorValue(value = "Sub_Service")
 public class SubService extends Service{
     @Column
     private Double basePrice;
