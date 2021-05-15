24bc62738b0271c35a22c85cf8d37e0ac842779c
zahra.asgari <zahra.asgari1996@yahoo.com>
2021-05-16 00:56:00 +0430

    crud for service.java

--- src/main/java/ir/maktab/data/repository/ServiceRepository.java
+++ src/main/java/ir/maktab/data/repository/ServiceRepository.java
@@ -1,4 +1,10 @@
 package ir.maktab.data.repository;
 
+import ir.maktab.data.domain.Service;
+
 public interface ServiceRepository {
+    void saveNewService(Service service);
+    Service findService(Service service);
+    void  deleteService(Service service);
+    void updateService(Service service);
 }
--- src/main/java/ir/maktab/data/repository/ServiceRepositoryImpl.java
+++ src/main/java/ir/maktab/data/repository/ServiceRepositoryImpl.java
@@ -1,4 +1,52 @@
 package ir.maktab.data.repository;
 
+import ir.maktab.data.domain.Service;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.stereotype.Repository;
+
+@Repository
 public class ServiceRepositoryImpl implements  ServiceRepository{
+    private final SessionFactory sessionFactory;
+
+    public ServiceRepositoryImpl(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
+    }
+
+    @Override
+    public void saveNewService(Service service) {
+        Session session= sessionFactory.openSession();
+        session.beginTransaction();
+        session.save(service);
+        session.getTransaction().commit();
+        session.close();
+    }
+
+    @Override
+    public Service findService(Service service) {
+        Session session= sessionFactory.openSession();
+        session.beginTransaction();
+        Service service1=session.get(Service.class,service.getId());
+        session.getTransaction().commit();
+        session.close();
+        return service1;
+    }
+
+    @Override
+    public void deleteService(Service service) {
+        Session session= sessionFactory.openSession();
+        session.beginTransaction();
+        session.delete(service);
+        session.getTransaction().commit();
+        session.close();
+    }
+
+    @Override
+    public void updateService(Service service) {
+        Session session= sessionFactory.openSession();
+        session.beginTransaction();
+        session.update(service);
+        session.getTransaction().commit();
+        session.close();
+    }
 }
