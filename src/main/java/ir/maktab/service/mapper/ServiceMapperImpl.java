6a6a86912a2b57616c6023c2fd14af264f7c1a38
zahra.asgari <zahra.asgari1996@yahoo.com>
2021-05-16 23:31:49 +0430

    complete mapper class for Service.java and SubService.java

--- src/main/java/ir/maktab/service/mapper/ServiceMapperImpl.java
+++ src/main/java/ir/maktab/service/mapper/ServiceMapperImpl.java
@@ -1,4 +1,26 @@
 package ir.maktab.service.mapper;
 
+import ir.maktab.data.domain.Service;
+import ir.maktab.dto.ServiceDto;
+import org.springframework.stereotype.Component;
+
+@Component
 public class ServiceMapperImpl implements ServiceMapper{
+    @Override
+    public ServiceDto convertToServiceDto(Service service) {
+        ServiceDto serviceDto=new ServiceDto();
+        serviceDto.setId(service.getId());
+        serviceDto.setName(service.getName());
+        serviceDto.setSubServices(service.getSubServices());
+        return serviceDto;
+    }
+
+    @Override
+    public Service convertToService(ServiceDto serviceDto) {
+        Service service=new Service();
+        service.setId(serviceDto.getId());
+        service.setName(serviceDto.getName());
+        service.setSubServices(serviceDto.getSubServices());
+        return service;
+    }
 }
--- src/main/java/ir/maktab/service/mapper/SubServiceMapperImpl.java
+++ src/main/java/ir/maktab/service/mapper/SubServiceMapperImpl.java
@@ -1,4 +1,31 @@
 package ir.maktab.service.mapper;
 
+import ir.maktab.data.domain.Service;
+import ir.maktab.data.domain.SubService;
+import ir.maktab.dto.SubServiceDto;
+import org.springframework.stereotype.Component;
+
+@Component
 public class SubServiceMapperImpl  implements SubServiceMapper{
+    @Override
+    public SubServiceDto covertToSubServiceDto(SubService subService) {
+        SubServiceDto subServiceDto=new SubServiceDto();
+        subServiceDto.setId(subService.getId());
+        subServiceDto.setName(subService.getName());
+        subServiceDto.setBasePrice(subService.getBasePrice());
+        subServiceDto.setDescription(subService.getDescription());
+        subServiceDto.setService(subService.getService());
+        return subServiceDto;
+    }
+
+    @Override
+    public SubService convertToSubService(SubServiceDto subServiceDto) {
+        SubService subService=new SubService();
+        subService.setId(subServiceDto.getId());
+        subService.setName(subServiceDto.getName());
+        subService.setBasePrice(subServiceDto.getBasePrice());
+        subService.setDescription(subServiceDto.getDescription());
+        subService.setService(subServiceDto.getService());
+        return subService;
+    }
 }
