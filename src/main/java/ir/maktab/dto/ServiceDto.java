ec860ba709eccdd59592e657b1c19969e234d884
zahra.asgari <zahra.asgari1996@yahoo.com>
2021-05-16 23:39:36 +0430

    completer dto for Service.java and SubService.java

--- src/main/java/ir/maktab/dto/ServiceDto.java
+++ src/main/java/ir/maktab/dto/ServiceDto.java
@@ -1,4 +1,40 @@
 package ir.maktab.dto;
 
+import ir.maktab.data.domain.SubService;
+import ir.maktab.data.enums.TypeOfService;
+
+import javax.persistence.Column;
+import javax.persistence.Enumerated;
+import javax.persistence.OneToMany;
+import java.util.ArrayList;
+import java.util.List;
+
 public class ServiceDto {
+    private Integer id;
+    private String name;
+    private List<SubService> subServices=new ArrayList<>();
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<SubService> getSubServices() {
+        return subServices;
+    }
+
+    public void setSubServices(List<SubService> subServices) {
+        this.subServices = subServices;
+    }
 }
--- src/main/java/ir/maktab/dto/SubServiceDto.java
+++ src/main/java/ir/maktab/dto/SubServiceDto.java
@@ -1,4 +1,52 @@
 package ir.maktab.dto;
 
+import ir.maktab.data.domain.Service;
+
+
 public class SubServiceDto {
+    private Integer id;
+    private Double basePrice;
+    private String description;
+    private String name;
+    private Service service;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Double getBasePrice() {
+        return basePrice;
+    }
+
+    public void setBasePrice(Double basePrice) {
+        this.basePrice = basePrice;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Service getService() {
+        return service;
+    }
+
+    public void setService(Service service) {
+        this.service = service;
+    }
 }
