a6026417a4ad56c03b6e1cfa844dc5061e2b9fa4
zahra.asgari <zahra.asgari1996@yahoo.com>
2021-05-16 23:16:19 +0430

    delete inheritance between service and subservice

--- src/main/java/ir/maktab/data/domain/Service.java
+++ src/main/java/ir/maktab/data/domain/Service.java
@@ -3,17 +3,39 @@ package ir.maktab.data.domain;
 import ir.maktab.data.enums.TypeOfService;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
-@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
-@DiscriminatorColumn(name = "Type_Of_Service")
-@DiscriminatorValue(value = "Service")
+//@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+//@DiscriminatorColumn(name = "Type_Of_Service")
+//@DiscriminatorValue(value = "Service")
 public class Service {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     @Enumerated
     private TypeOfService type;
+    @Column
+    private String name;
+    @OneToMany
+    private List<SubService> subServices=new ArrayList<>();
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<SubService> getSubServices() {
+        return subServices;
+    }
+
+    public void setSubServices(List<SubService> subServices) {
+        this.subServices = subServices;
+    }
 
     public Integer getId() {
         return id;
--- src/main/java/ir/maktab/data/domain/SubService.java
+++ src/main/java/ir/maktab/data/domain/SubService.java
@@ -1,18 +1,37 @@
 package ir.maktab.data.domain;
 
-import javax.persistence.Column;
-import javax.persistence.DiscriminatorValue;
-import javax.persistence.Entity;
+import javax.persistence.*;
 
 @Entity
-@DiscriminatorValue(value = "Sub_Service")
-public class SubService extends Service{
+//@DiscriminatorValue(value = "Sub_Service")
+public class SubService {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
     @Column
     private Double basePrice;
     @Column
     private String description;
     @Column
     private String name;
+    @ManyToOne
+    private Service service;
+
+    public Service getService() {
+        return service;
+    }
+
+    public void setService(Service service) {
+        this.service = service;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
 
     public Double getBasePrice() {
         return basePrice;
