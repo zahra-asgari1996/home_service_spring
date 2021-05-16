38cfc2382e7a91fb99ca3b27f84ac30916fcc401
zahra.asgari <zahra.asgari1996@yahoo.com>
2021-05-16 23:38:49 +0430

    complete save method in service and controller layer  for Service.java and SubService.java

--- src/main/java/ir/maktab/web/ServiceController.java
+++ src/main/java/ir/maktab/web/ServiceController.java
@@ -1,4 +1,19 @@
 package ir.maktab.web;
 
+import ir.maktab.dto.ServiceDto;
+import ir.maktab.service.ServiceService;
+import org.springframework.stereotype.Controller;
+
+@Controller
 public class ServiceController {
+    private final ServiceService service;
+
+
+    public ServiceController(ServiceService service) {
+        this.service = service;
+    }
+
+    public void saveNewService(ServiceDto serviceDto){
+        service.saveNewService(serviceDto);
+    }
 }
--- src/main/java/ir/maktab/web/SubServiceController.java
+++ src/main/java/ir/maktab/web/SubServiceController.java
@@ -1,4 +1,18 @@
 package ir.maktab.web;
 
+import ir.maktab.dto.SubServiceDto;
+import ir.maktab.service.SubServiceService;
+import org.springframework.stereotype.Controller;
+
+@Controller
 public class SubServiceController {
+    private final SubServiceService subServiceService;
+
+    public SubServiceController(SubServiceService subServiceService) {
+        this.subServiceService = subServiceService;
+    }
+
+    public void saveNewSubService(SubServiceDto subServiceDto){
+        subServiceService.saveNewSubService(subServiceDto);
+    }
 }
