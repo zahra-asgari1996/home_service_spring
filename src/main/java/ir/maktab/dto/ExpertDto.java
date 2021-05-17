2c158f96e228482947700287fccfcfab73abb110
zahra.asgari <zahra.asgari1996@yahoo.com>
2021-05-17 19:00:55 +0430

    complete ExpertDto.java.java properties

--- src/main/java/ir/maktab/dto/ExpertDto.java
+++ src/main/java/ir/maktab/dto/ExpertDto.java
@@ -1,4 +1,39 @@
 package ir.maktab.dto;
 
+import ir.maktab.data.domain.SubService;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class ExpertDto extends UserDto{
+    private Integer rate;
+    private byte[] image;
+    private List<SubService> services=new ArrayList<>();
+
+    public Integer getRate() {
+        return rate;
+    }
+
+    public ExpertDto setRate(Integer rate) {
+        this.rate = rate;
+        return this;
+    }
+
+    public byte[] getImage() {
+        return image;
+    }
+
+    public ExpertDto setImage(byte[] image) {
+        this.image = image;
+        return this;
+    }
+
+    public List<SubService> getServices() {
+        return services;
+    }
+
+    public ExpertDto setServices(List<SubService> services) {
+        this.services = services;
+        return this;
+    }
 }
