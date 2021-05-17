344e92ac40cb40e55f628aa9e33fc964ab7a2616
zahra.asgari <zahra.asgari1996@yahoo.com>
2021-05-18 01:40:17 +0430

    crud for Expert.java

--- src/main/java/ir/maktab/data/repository/ExpertRepository.java
+++ src/main/java/ir/maktab/data/repository/ExpertRepository.java
@@ -2,6 +2,11 @@ package ir.maktab.data.repository;
 
 import ir.maktab.data.domain.Expert;
 
+import java.util.List;
+
 public interface ExpertRepository {
     void saveNewExpert(Expert expert);
+    void deleteExpert(Expert expert);
+    void updateExpert(Expert expert);
+    List<Expert> fetchAllExperts();
 }
--- src/main/java/ir/maktab/data/repository/ExpertRepositoryImpl.java
+++ src/main/java/ir/maktab/data/repository/ExpertRepositoryImpl.java
@@ -1,9 +1,13 @@
 package ir.maktab.data.repository;
 
+import ir.maktab.data.domain.Customer;
 import ir.maktab.data.domain.Expert;
+import org.hibernate.Criteria;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public class ExpertRepositoryImpl implements ExpertRepository{
@@ -20,5 +24,34 @@ public class ExpertRepositoryImpl implements ExpertRepository{
         session.save(expert);
         session.getTransaction().commit();
         session.close();
+    }
+
+    @Override
+    public void deleteExpert(Expert expert) {
+        Session session= sessionFactory.openSession();
+        session.beginTransaction();
+        session.delete(expert);
+        session.getTransaction().commit();
+        session.close();
+    }
+
+    @Override
+    public void updateExpert(Expert expert) {
+        Session session= sessionFactory.openSession();
+        session.beginTransaction();
+        session.update(expert);
+        session.getTransaction().commit();
+        session.close();
+    }
+
+    @Override
+    public List<Expert> fetchAllExperts() {
+        Session session= sessionFactory.openSession();
+        session.beginTransaction();
+        Criteria criteria = session.createCriteria(Expert.class);
+        List<Expert> list = criteria.list();
+        session.getTransaction().commit();
+        session.close();
+        return list;
     }
 }
