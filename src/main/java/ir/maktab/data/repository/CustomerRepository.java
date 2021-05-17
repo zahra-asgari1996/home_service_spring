8f241c93a59e5ee3807a84ca9f8ee6212baeb65d
zahra.asgari <zahra.asgari1996@yahoo.com>
2021-05-18 01:40:37 +0430

    crud for Customer.java

--- src/main/java/ir/maktab/data/repository/CustomerRepository.java
+++ src/main/java/ir/maktab/data/repository/CustomerRepository.java
@@ -1,9 +1,15 @@
 package ir.maktab.data.repository;
 
+import com.google.protobuf.CodedInputStream;
 import ir.maktab.data.domain.Customer;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 
 public interface CustomerRepository {
     void saveNewCustomer(Customer customer);
+    void updateCustomer(Customer customer);
+    void deleteCustomer(Customer customer);
+    List<Customer> fetchAllCustomers();
 }
--- src/main/java/ir/maktab/data/repository/CustomerRepositoryImpl.java
+++ src/main/java/ir/maktab/data/repository/CustomerRepositoryImpl.java
@@ -1,9 +1,12 @@
 package ir.maktab.data.repository;
 
 import ir.maktab.data.domain.Customer;
+import org.hibernate.Criteria;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public class CustomerRepositoryImpl implements CustomerRepository{
@@ -20,5 +23,35 @@ public class CustomerRepositoryImpl implements CustomerRepository{
         session.save(customer);
         session.getTransaction().commit();
         session.close();
+    }
+
+    @Override
+    public void updateCustomer(Customer customer) {
+        Session session= sessionFactory.openSession();
+        session.beginTransaction();
+        session.update(customer);
+        session.getTransaction().commit();
+        session.close();
+    }
+
+    @Override
+    public void deleteCustomer(Customer customer) {
+        Session session= sessionFactory.openSession();
+        session.beginTransaction();
+        session.delete(customer);
+        session.getTransaction().commit();
+        session.close();
+
+    }
+
+    @Override
+    public List<Customer> fetchAllCustomers() {
+        Session session= sessionFactory.openSession();
+        session.beginTransaction();
+        Criteria criteria = session.createCriteria(Customer.class);
+        List<Customer> list = criteria.list();
+        session.getTransaction().commit();
+        session.close();
+        return list;
     }
 }
