2304666bb2fa0de860293e19c5ca4c3a9e7dbd31
zahra.asgari <zahra.asgari1996@yahoo.com>
2021-05-17 18:59:41 +0430

    complete UserDto.java properties

--- src/main/java/ir/maktab/dto/UserDto.java
+++ src/main/java/ir/maktab/dto/UserDto.java
@@ -1,4 +1,83 @@
 package ir.maktab.dto;
 
+import ir.maktab.data.enums.Situation;
+import org.hibernate.annotations.CreationTimestamp;
+
+import javax.persistence.Column;
+import javax.persistence.Enumerated;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import java.util.Date;
+
 public class UserDto {
+    private Integer id;
+    private String name;
+    private String lastName;
+    private String email;
+    private String password;
+    private Situation situation;
+    private Date date;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public UserDto setId(Integer id) {
+        this.id = id;
+        return this;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public UserDto setName(String name) {
+        this.name = name;
+        return this;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public UserDto setLastName(String lastName) {
+        this.lastName = lastName;
+        return this;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public UserDto setEmail(String email) {
+        this.email = email;
+        return this;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public UserDto setPassword(String password) {
+        this.password = password;
+        return this;
+    }
+
+    public Situation getSituation() {
+        return situation;
+    }
+
+    public UserDto setSituation(Situation situation) {
+        this.situation = situation;
+        return this;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public UserDto setDate(Date date) {
+        this.date = date;
+        return this;
+    }
 }
