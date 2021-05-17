17a854346f28b8c43226d37fc6a2c722c52fce13
zahra.asgari <zahra.asgari1996@yahoo.com>
2021-05-18 01:39:55 +0430

    crud for Manager.java

--- src/main/java/ir/maktab/data/repository/ManagerRepository.java
+++ src/main/java/ir/maktab/data/repository/ManagerRepository.java
@@ -1,4 +1,12 @@
 package ir.maktab.data.repository;
 
+import ir.maktab.data.domain.Manager;
+
+import java.util.List;
+
 public interface ManagerRepository {
+    void saveNewManager(Manager manager);
+    void deleteManager(Manager manager);
+    void updateManager(Manager manager);
+    List<Manager> fetchAllManagers();
 }
