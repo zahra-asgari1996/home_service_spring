ace6c1a4f117508180bf42f9afb0fe521efaea81
zahra.asgari <zahra.asgari1996@yahoo.com>
2021-05-17 18:57:38 +0430

    create save method for expert

--- src/main/java/ir/maktab/data/repository/ExpertRepository.java
+++ src/main/java/ir/maktab/data/repository/ExpertRepository.java
@@ -1,4 +1,7 @@
 package ir.maktab.data.repository;
 
+import ir.maktab.data.domain.Expert;
+
 public interface ExpertRepository {
+    void saveNewExpert(Expert expert);
 }
--- src/main/java/ir/maktab/data/repository/ExpertRepositoryImpl.java
+++ src/main/java/ir/maktab/data/repository/ExpertRepositoryImpl.java
@@ -1,4 +1,24 @@
 package ir.maktab.data.repository;
 
-public class ExpertRepositoryImpl {
+import ir.maktab.data.domain.Expert;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class ExpertRepositoryImpl implements ExpertRepository{
+    private final SessionFactory sessionFactory;
+
+    public ExpertRepositoryImpl(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
+    }
+
+    @Override
+    public void saveNewExpert(Expert expert) {
+        Session session= sessionFactory.openSession();
+        session.beginTransaction();
+        session.save(expert);
+        session.getTransaction().commit();
+        session.close();
+    }
 }
