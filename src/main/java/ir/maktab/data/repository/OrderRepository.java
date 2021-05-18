2ce34f3879a2ab184d755854f4db934fe30b19ca
zahra.asgari <zahra.asgari1996@yahoo.com>
2021-05-18 11:47:48 +0430

    crud for Orders.java

--- src/main/java/ir/maktab/data/repository/OrderRepository.java
+++ src/main/java/ir/maktab/data/repository/OrderRepository.java
@@ -1,4 +1,12 @@
 package ir.maktab.data.repository;
 
+import ir.maktab.data.domain.Orders;
+
+import java.util.List;
+
 public interface OrderRepository {
+    void saveNewOrder(Orders orders);
+    void deleteOrder(Orders orders);
+    void updateOrder(Orders orders);
+    List<Orders> fetchAllOrders();
 }
