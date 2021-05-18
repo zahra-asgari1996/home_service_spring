0724dd3db36648758128634a3005c9e1bb49244e
zahra.asgari <zahra.asgari1996@yahoo.com>
2021-05-18 11:42:22 +0430

    add order list for Customer.java

--- src/main/java/ir/maktab/data/domain/Customer.java
+++ src/main/java/ir/maktab/data/domain/Customer.java
@@ -4,7 +4,13 @@ import ir.maktab.data.enums.Role;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Customer extends Users {
+
+    @OneToMany(mappedBy = "customer")
+    private List<Orders> orders=new ArrayList<>();
 }
