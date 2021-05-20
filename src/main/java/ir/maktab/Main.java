faaad3559823a68a6162a5b579300c6d86316040
zahra.asgari <zahra.asgari1996@yahoo.com>
2021-05-21 00:16:19 +0430

    class

--- src/main/java/ir/maktab/Main.java
+++ src/main/java/ir/maktab/Main.java
@@ -4,6 +4,7 @@ import ir.maktab.configuration.Config;
 import ir.maktab.data.domain.Customer;
 import ir.maktab.data.domain.Service;
 import ir.maktab.data.repository.CustomerRepository;
+import ir.maktab.data.repository.UserRepository;
 import ir.maktab.dto.ServiceDto;
 import ir.maktab.web.ServiceController;
 import org.springframework.context.ApplicationContext;
@@ -19,6 +20,9 @@ public class Main {
         customer.setEmail("aaaaa");
         customer.setPassword("111");
         customerRepository.saveNewCustomer(customer);
+        customer.setPassword("22222");
+        UserRepository userRepository=iocContainer.getBean(UserRepository.class);
+        userRepository.changePassword(customer);
 //        ServiceDto service=new ServiceDto();
 //        service.setName("a");
 //        ServiceController serviceController=iocContainer.getBean(ServiceController.class);
