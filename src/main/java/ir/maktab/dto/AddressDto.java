03ecf0e5787ff9c7424d23240f86c0264edfcac9
zahra.asgari <zahra.asgari1996@yahoo.com>
2021-05-20 13:31:30 +0430

    create address class

---
+++ src/main/java/ir/maktab/dto/AddressDto.java
@@ -0,0 +1,10 @@
+package ir.maktab.dto;
+
+import javax.persistence.Column;
+
+public class AddressDto {
+    private String city;
+    private String street;
+    private String alley;
+    private String plaque;
+}
