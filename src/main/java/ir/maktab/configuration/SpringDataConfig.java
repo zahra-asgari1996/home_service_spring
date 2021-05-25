6e69606810dd8127dbd749f8d6d3b44951664736
zahra.asgari <zahra.asgari1996@yahoo.com>
2021-05-26 00:07:00 +0430

    spring data config files

--- src/main/java/ir/maktab/configuration/SpringDataConfig.java
+++ src/main/java/ir/maktab/configuration/SpringDataConfig.java
@@ -3,16 +3,22 @@ package ir.maktab.configuration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
 import java.util.Properties;
 
 @Configuration
+@EnableTransactionManagement
+@EnableJpaRepositories(basePackages = {
+        "ir.maktab.data.repository"
+})
 public class SpringDataConfig {
     private final Environment environment;
 
