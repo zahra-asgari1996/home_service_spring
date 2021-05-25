3f74772c8db78d953ab35c892c286b978502ebc8
zahra.asgari <zahra.asgari1996@yahoo.com>
2021-05-26 00:02:53 +0430

    spring data config files

---
+++ src/main/java/ir/maktab/configuration/SpringDataConfig.java
@@ -0,0 +1,54 @@
+package ir.maktab.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+
+import javax.persistence.EntityManagerFactory;
+import javax.sql.DataSource;
+import java.util.Properties;
+
+@Configuration
+public class SpringDataConfig {
+    private final Environment environment;
+
+    public SpringDataConfig(Environment environment) {
+        this.environment = environment;
+    }
+
+    @Bean
+    public DataSource getDataSource(){
+        DriverManagerDataSource dataSource=new DriverManagerDataSource();
+        dataSource.setDriverClassName(environment.getRequiredProperty("hibernate.connection.driver_class"));
+        dataSource.setUrl(environment.getRequiredProperty("hibernate.connection.url"));
+        dataSource.setUsername(environment.getRequiredProperty("hibernate.connection.username"));
+        dataSource.setPassword(environment.getRequiredProperty("hibernate.connection.password"));
+        return dataSource;
+    }
+
+    @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactoryBean(DataSource dataSource){
+        LocalContainerEntityManagerFactoryBean entityManagerFactoryBean
+                =new LocalContainerEntityManagerFactoryBean();
+        entityManagerFactoryBean.setDataSource(dataSource);
+        entityManagerFactoryBean.setJpaVendorAdapter(new HibernateJpaVendorAdapter());
+        entityManagerFactoryBean.setPackagesToScan("ir.maktab.data.domain");
+        Properties properties=new Properties();
+        properties.put("hibernate.hbm2ddl.auto",environment.getRequiredProperty("hibernate.hbm2ddl.auto"));
+        properties.put("hibernate.show_sql",environment.getRequiredProperty("hibernate.show_sql"));
+        properties.put("hibernate.format_sql",environment.getRequiredProperty("hibernate.format_sql"));
+        entityManagerFactoryBean.setJpaProperties(properties);
+        return  entityManagerFactoryBean;
+    }
+
+    @Bean
+    public JpaTransactionManager transactionManager(EntityManagerFactory entityManagerFactory){
+        JpaTransactionManager transactionManager=new JpaTransactionManager();
+        transactionManager.setEntityManagerFactory(entityManagerFactory);
+        return transactionManager;
+    }
+}
